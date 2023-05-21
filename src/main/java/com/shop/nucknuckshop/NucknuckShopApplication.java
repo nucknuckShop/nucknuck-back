1f23723262d986d7ff7e065e2765110dc216540e
closeup <closeup1202@gmail.com>
2023-05-21 20:12:14 +0900

    first-commit

---
+++ src/main/java/com/shop/nucknuckshop/NucknuckShopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.nucknuckshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucknuckShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NucknuckShopApplication.class, args);
+    }
+
+}
